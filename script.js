f812b2a7f31e97181c1e863adcabe284393f144c
Ranjan <RanjanKumar.Choubey@walmart.com>
2026-04-01 14:11:25 +0530

    updated v2.0

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// here is yje comment
+// Smooth scroll for nav links (polyfill for older browsers)
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    const target = document.querySelector(link.getAttribute('href'));
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
